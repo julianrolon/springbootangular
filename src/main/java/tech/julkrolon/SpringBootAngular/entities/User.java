c8c7d957c743c5ea964ae9ad2790d93ffd5fec94
julianrolon <julianrolonramirez@gmail.com>
2022-10-03 14:44:48 -0500

    JPA Entity

---
+++ src/main/java/tech/julkrolon/SpringBootAngular/entities/User.java
@@ -0,0 +1,24 @@
+package tech.julkrolon.SpringBootAngular.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private String email;
+}
