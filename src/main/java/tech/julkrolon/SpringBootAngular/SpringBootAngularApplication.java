42e9add101aa87de1ab1d88d0ab3c791a20bf5f0
julianrolon <julianrolonramirez@gmail.com>
2022-10-03 14:37:51 -0500

    Spring boot project

---
+++ src/main/java/tech/julkrolon/SpringBootAngular/SpringBootAngularApplication.java
@@ -0,0 +1,13 @@
+package tech.julkrolon.SpringBootAngular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/julkrolon/SpringBootAngular/SpringBootAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.julkrolon.SpringBootAngular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
